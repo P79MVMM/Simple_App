2b213b424cbcf4446bfb3974ab5807fc8d3b074b
P79MVMM <110417819+P79MVMM@users.noreply.github.com>
2022-08-08 13:52:05 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const {getByText} = render(<App available={true} />);
+  expect(/learn react/i)).toBeInTheDocument();
 });
+//Previous one
+//render(<App />);
+//const linkElement = screen.getByText(/learn react/i);
+//expect(linkElement).toBeInTheDocument();
